643b01b05036dc777dd445f12e8928b3d57a042e
Ritank Jaikar <ritank.jaikar@gmail.com>
2025-01-01 06:40:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
+	}
 
-class Student extends Person {}
+	get name() {
+		return this._name;
+	}
 
-class Teacher extends Person {}
+	set age(age) {
+		if (typeof age === 'number' && age >= 0) {
+	      this._age = age;
+	    } else {
+	      throw new Error("Age must be a non-negative number.");
+	    }
+	}
+}
+
+class Student extends Person {
+	
+	
+	function study() {
+		console.log(`${this.name} is studying`)
+	}
+}
+
+class Teacher extends Person {
+	function teach() {
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
